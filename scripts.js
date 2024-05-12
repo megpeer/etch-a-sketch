244ece7086578c9d2fb96fcb11dd74953ebbbd8c
Meg Peer <megimu@gmail.com>
2024-05-12 09:08:33 -0400

    size of pixels and flex styling now fits the drawing box

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 
-let gridSize = 256;
 // query selector container
 const drawingField = document.querySelector("#container");
+
+//temporary grid size
+let gridSize = 256;
 
 // create div with id "pixel"
 //append pixel to container (might need to use a for loop to make 16x16 divs)
