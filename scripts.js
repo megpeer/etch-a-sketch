5e0c0dd59c937bf4e22cba6c31f244f25077544c
Meg Peer <megimu@gmail.com>
2024-05-12 08:51:13 -0400

    pixels for drawing field generation, now functions via for loop with static input

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
+
+let gridSize = 256;
 // query selector container
 const drawingField = document.querySelector("#container");
 
 // create div with id "pixel"
-let pixel = document.createElement("div");
-pixel.className = "pixel";
-
 //append pixel to container (might need to use a for loop to make 16x16 divs)
-// for (let i = 0; i = 16; i++)
-drawingField.appendChild(pixel);
+for (i = 0; i < gridSize; i++){
+    let pixel = document.createElement("div");
+    pixel.className = "pixel";
+    drawingField.appendChild(pixel);
+
+}
 
