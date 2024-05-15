68bde024ad643f951803937fe9e1320d36252a60
Meg Peer <megimu@gmail.com>
2024-05-14 22:14:28 -0400

    pixel resize response added

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 
 const drawingField = document.querySelector("#container");
-let size = 10;
+let size;
 
-function createGrid() {
+function createGrid(size) {
     for (i = 0; i < size * size; i++){
-        let pixel = document.createElement("div");
-        pixel.style.height = `calc(100%/${size})`;
-        pixel.style.width = `calc(100%/${size})`;
+        let pixel = document.createElement("div"); 
+        drawingField.appendChild(pixel);
+        pixel.style.height = ((400/size) + "px");
+        pixel.style.width = ((400/size) + "px");
         pixel.className = "pixel";
         pixel.id = "inactive";
-        drawingField.appendChild(pixel);
         pixel.onmouseover = function () {
         pixel.style.backgroundColor = "black";
         }
@@ -18,18 +18,20 @@ function createGrid() {
     }
 };
 
-// button creates alert, that changes amount of pixels appearing int he container, and what size
 let button = document.querySelector("#input") 
 button.addEventListener("click", input);
 
 function input() {
-    let size = prompt("input pixel size", "10") 
+    let size = parseInt(prompt("input a number between 1 and 100"))
+    container.innerHTML = "";
     
-        if (size > 100 || size === 0) {
-            alert("must be between 1-100")
+        if (size <= 100 || size > 0) {
+            
+            createGrid(size)
+
         }
-        else {
-            createGrid() 
-        }
+        else 
+            alert("please enter a number between 1-100")
+   
     };
 
